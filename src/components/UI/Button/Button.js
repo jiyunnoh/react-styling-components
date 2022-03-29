aa02910446797d0d067b44f1d0f38bdc7995e998
jiyunnoh <ianjy1127@gmail.com>
2022-03-28 23:48:39 -0500

    80-81. Using CSS Modules with Dynamic Styles

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,44 +1,45 @@
-import styled from 'styled-components';
+import React from 'react';
+import styles from './Button.module.css';
+// import styled from 'styled-components';
 
 // attacked template literal (``) is a default JavaScript syntax.
 // Things inside backtics will be passed into the method(button()) in a special way,
 // and will be executed just like as a method.
 // styled.button`` will return a new button component with styles.
-const Button = styled.button`
-  // remove any selectors (.button) inside backticks.
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   // remove any selectors (.button) inside backticks.
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  @media (min-width: 765px) {
-    width: auto;
-  }
+//   @media (min-width: 765px) {
+//     width: auto;
+//   }
 
-  // for seudo-selectors, use &
-  &:focus {
-    outline: none;
-  }
+//   // for seudo-selectors, use &
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
